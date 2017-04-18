8f5338c9f4809c177292a228f33b57bbfcdbaaa7
bperraud <baptiste.perraud@orange.fr>
2017-04-18 16:04:56 +0200

    Default text when no task has been created yet + some visual enhancement

--- nf28TD06/app/src/main/java/com/example/nf28/todolist/ShowTasksActivity.java
+++ nf28TD06/app/src/main/java/com/example/nf28/todolist/ShowTasksActivity.java
@@ -1,11 +1,14 @@
 package com.example.nf28.todolist;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
@@ -15,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class ShowTasksActivity extends AppCompatActivity {
+public class ShowTasksActivity extends ListActivity {
 
     private AdapterTask adapter;
 
@@ -34,38 +37,11 @@ public class ShowTasksActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_tasks);
 
         List<Task> tasksList = new ArrayList<>();
-        try {
-            tasksList.add(
-                    new Task(
-                            "Aller en EI03",
-                            Task.BEFORE_STATUS,
-                            Task.HIGH_PRIORITY,
-                            DateFormat.getDateInstance(DateFormat.SHORT).parse("16/04/2017")
-                    )
-            );
-            tasksList.add(
-                    new Task(
-                            "Donner la bouffe à Paf",
-                            Task.BEFORE_STATUS,
-                            Task.MEDIUM_PRIORITY,
-                            DateFormat.getDateInstance(DateFormat.SHORT).parse("20/04/2017")
-                    )
-            );
-            tasksList.add(
-                    new Task(
-                            "Téléphoner à mamie, mais cette tâche est tellement longue qu'elle s'étend sur plusieurs lignes nom de Dieu !",
-                            Task.BEFORE_STATUS,
-                            Task.LOW_PRIORITY,
-                            DateFormat.getDateInstance(DateFormat.SHORT).parse("16/04/2017")
-                    )
-            );
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
 
-        ListView tasksListView = (ListView) findViewById(R.id.tasksListView);
+        ListView tasksListView = (ListView) findViewById(android.R.id.list);
         adapter = new AdapterTask(this, R.layout.row_task, tasksList);
         tasksListView.setAdapter(adapter);
+        tasksListView.setEmptyView(findViewById(android.R.id.empty));
 
         Button addButton = (Button) findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
